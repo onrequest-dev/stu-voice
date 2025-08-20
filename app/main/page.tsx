7209905770fe9330399594b4c718c86642aba376
hadiikaddour <hadyqdwr77@gmail.com>
2025-08-20 14:18:45 +0300

    pwa2

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,17 +1,8 @@
 import MainInterface from "@/components/MainInterface";
-import Head from "next/head";
+
 const page = () => {
    return (
     <>
-    <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="My App" />
-      </Head>
     <MainInterface/>
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,20 @@ import React from 'react';
 import WelcomeHeader from '../components/WelcomeHeader';
 import FeaturesSection from '../components/FeaturesSection';
 import ContributionSection from '../components/ContributionSection';
+import Head from 'next/head';
 const page = () => {
    return (
+
+    <>
+    <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="My App" />
+      </Head>
     <div className="flex flex-col min-h-screen">
       {/* الهيدر الترحيبي */}
       <div className="flex-grow">
@@ -22,6 +34,7 @@ const page = () => {
       </div>
 
     </div>
+    </>
   );
 };
 
