474d989f74eaa21bfd2fd92a00a7d0a1d30f1219
hadiikaddour <hadyqdwr77@gmail.com>
2025-08-20 14:26:28 +0300

    stuvoice

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,7 @@ import Head from 'next/head';
 const page = () => {
    return (
 
-    <>
-    <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="My App" />
-      </Head>
+    
     <div className="flex flex-col min-h-screen">
       {/* الهيدر الترحيبي */}
       <div className="flex-grow">
@@ -34,7 +25,6 @@ const page = () => {
       </div>
 
     </div>
-    </>
   );
 };
 
