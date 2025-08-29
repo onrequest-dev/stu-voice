bd6fc8ed22072bb624bcc35d657199b5dfa43805
hadiikaddour <hadyqdwr77@gmail.com>
2025-08-29 20:29:06 +0300

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from './Providers'; 
-import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +11,14 @@ export const metadata: Metadata = {
   description: "رأيك يصنع الفرق",
 };
 
+
+
+export const viewport: Viewport = {
+  maximumScale: 1,
+  userScalable: false
+}
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,21 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar">
-      <Head>
-        {/* PWA Manifest */}
-        <link rel="manifest" href="/manifest.json" />
-
-        {/* أيقونات */}
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="icon" href="/icons/icon-192x192.png" />
-
-        {/* ألوان وخصائص الهاتف */}
-        <meta name="theme-color" content="#000000" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="STUvoice" />
-      </Head>
       <body className={inter.className}>
         <Providers>
           {children}
